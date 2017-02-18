513c0f6b6d7c9bfe709ae3e8612a0aead1b03bbe
ayoub <ayoub.sbai@student.ecp.fr>
2017-02-18 23:47:19 +0100

    Little better greedy

--- problem-02/submissions/ayoub-greedy.py
+++ problem-02/submissions/ayoub-greedy.py
@@ -7,7 +7,7 @@ class AyoubGreedySubmission(Submission):
         return 'ayoub-greedy'
 
     def run(self, input):
-        graph, s = input
+        graph, start = input
 
         def closest(current, exceptions=set()):
             m = float('inf')
@@ -18,30 +18,42 @@ class AyoubGreedySubmission(Submission):
                     c = v
             return c
 
-        path = [s]
-        current = s
-        visited = { s }
+        def traversal(s):
+            path = [s]
+            current = s
+            visited = { s }
 
-        left = closest(current, visited)
-        visited.add(left)
-        right = closest(current, visited)
-        visited.add(right)
-
-        while left and right:
-            path.append(right)
-            path.insert(0, left)
-
-            left = closest(left, visited)
-            if not left:
-                break
+            left = closest(current, visited)
             visited.add(left)
-
-            right = closest(right, visited)
-            if not right:
-                break
+            right = closest(current, visited)
             visited.add(right)
 
-        d = path.index(s)
-        n = len(path)
+            while left and right:
+                path.append(right)
+                path.insert(0, left)
 
-        return [path[(i + d) % n] for i in range(n)] + [s]
+                left = closest(left, visited)
+                if not left:
+                    break
+                visited.add(left)
+
+                right = closest(right, visited)
+                if not right:
+                    break
+                visited.add(right)
+
+            d = path.index(start)
+            n = len(path)
+
+            return [path[(i + d) % n] for i in range(n)] + [start]
+
+        min_l = float('inf')
+        min_p = None
+        for s in graph:
+            path = traversal(s)
+            l = sum([graph[path[i]][path[i+1]] for i in range(len(path) - 1)])
+            if l < min_l:
+                min_l = l
+                min_p = path
+
+        return min_p
