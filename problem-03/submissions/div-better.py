fb6417b12e91519a10c91277c0ed5b29fae87645
David Dos Santos <david.dossantos@datadoghq.com>
2017-02-19 21:00:53 +0100

    [problem-03] div 2nd submission

---
+++ problem-03/submissions/div-better.py
@@ -0,0 +1,39 @@
+# internal deps
+from submission import Submission
+
+# 3rd party deps
+from itertools import permutations
+
+class David2Submission(Submission):
+    def author(self):
+        return 'div-2'
+
+
+    def run(self, graph):
+        DEPTH = 2
+
+        def length(graph, path):
+            """
+            return length of the given path
+            """
+            return sum(graph[x][y] for x,y in zip(path, path[1:]))
+
+
+        nb_nodes = len(graph.keys())
+
+        # noeuds à visiter
+        to_visit = set(graph.keys())
+
+        # path contient le chemin parcouru
+        path = []
+
+        # on y va en glouton un peu plus malin
+        # boucle principale : tant qu'il reste des noeuds à visiter
+        while len(path) < nb_nodes:
+
+            p = permutations(to_visit, r=min(len(to_visit), DEPTH))
+            subpath = list(min(p, key=lambda _path: length(graph, path + list(_path))))
+            to_visit = to_visit - set(subpath)
+            path += subpath
+
+        return path
